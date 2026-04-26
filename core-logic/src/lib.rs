64c16ee25dd692166be264b0a5557883e0e670f3
p4xxus <79471967+p4xxus@users.noreply.github.com>
2026-04-26 20:09:20 +0200

    added sha1hasher trait

--- core-logic/src/lib.rs
+++ core-logic/src/lib.rs
@@ -5,10 +5,12 @@ extern crate alloc;
 pub mod bittorrenter;
 pub mod core;
 pub mod fs;
+pub mod hash;
 pub mod net;
 mod peer;
 
 pub use bittorrenter::{BitTorrenter, error::BitTorrenterError};
 pub use core::metainfo::{Info, MetaInfoFile};
+pub use hash::Sha1Hasher;
 pub use net::tcp::TcpConnector;
 pub use peer::BLOCK_SIZE;
